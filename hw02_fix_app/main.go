028b85aa17209306310375d55247bd24f38e5cf3
lls <liseykin.a.v@gmail.com>
2024-11-25 14:47:22 +0300

    HW02 is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,29 +1,33 @@
-package init
+package main
 
 import (
+	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
-func init() {
-	var path string = "data.json"
+func main() {
+	path := "data.json"
 
 	fmt.Printf("Enter data file path: ")
-	fmt.Scanln(&path)
 
-	var err error
-	var staff []types.Employee
+	_, err := fmt.Scanln(&path)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
+	var staff []types.Employee
 
-	fmt.Print(err)
+	staff, err = reader.ReadJSON(path, -1)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 
 	printer.PrintStaff(staff)
 }
